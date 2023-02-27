79784697e49847312d5b01a587ec413b7b24f273
YuliaGrun <51439701+YuliaGrun@users.noreply.github.com>
2023-02-27 10:09:47 +0300

    Добавление viewModel
    Добавление LiveData, состояний приложения

--- app/src/main/java/com/example/filmsapp/view/MainFragment.kt
+++ app/src/main/java/com/example/filmsapp/view/MainFragment.kt
@@ -5,8 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.example.filmsapp.R
 import com.example.filmsapp.databinding.FragmentMainBinding
+import com.example.filmsapp.viewmodel.AppState
+import com.example.filmsapp.viewmodel.MainViewModel
 
 
 class MainFragment : Fragment() {
@@ -29,11 +34,32 @@ class MainFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val viewModel  = ViewModelProvider(this)[MainViewModel::class.java]
+        val observer = object : Observer<AppState> {
+            override fun onChanged(data: AppState) {
+                renderData(data)
+            }
+        }
+        viewModel.getData().observe(viewLifecycleOwner,observer)
+        viewModel.getFilms()
     }
     private val ui get() = _ui!!
 
     override fun onDestroyView() {
         super.onDestroyView()
         _ui = null
+    }
+    private fun renderData(data: AppState){
+        when(data){
+            is AppState.Error -> {
+                _ui!!.loadingLayout.visibility = View.GONE
+            }
+            is AppState.Loading -> {
+                _ui!!.loadingLayout.visibility = View.VISIBLE
+            }
+            is AppState.Success -> {
+                _ui!!.loadingLayout.visibility = View.GONE
+            }
+        }
     }
 }
