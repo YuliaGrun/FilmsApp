28c434cc1b55f4d125305edb570b8cb7d1dd3726
YuliaGrun <51439701+YuliaGrun@users.noreply.github.com>
2023-03-10 11:49:44 +0300

    Макет для фильмов
    Добавление макета для отображения фильмов

--- app/src/main/java/com/example/filmsapp/model/Film.kt
+++ app/src/main/java/com/example/filmsapp/model/Film.kt
@@ -6,11 +6,11 @@ data class Film (val filmName: String, val yearRelease: Int, val country: String
 
 fun getFilms(): List<Film> {
     return listOf(
-        Film("Top Gun: Maverick", 2022, "United States", R.drawable.ic_launcher_foreground),
-        Film("Avatar: The Way of Water", 2022, "United States", R.drawable.ic_launcher_foreground),
-        Film("Black Panther: Wakanda Forever", 2022, "United States", R.drawable.ic_launcher_foreground),
-        Film("Doctor Strange in the Multiverse of Madness", 2022, "United States", R.drawable.ic_launcher_foreground),
-        Film("Jurassic World Dominion", 2022, "United States", R.drawable.ic_launcher_foreground)
+        Film("Top Gun: Maverick", 2022, "United States", R.drawable.ic_films),
+        Film("Avatar: The Way of Water", 2022, "United States", R.drawable.ic_films),
+        Film("Black Panther: Wakanda Forever", 2022, "United States", R.drawable.ic_films),
+        Film("Doctor Strange in the Multiverse of Madness", 2022, "United States", R.drawable.ic_films),
+        Film("Jurassic World Dominion", 2022, "United States", R.drawable.ic_films)
 
     )
 }
